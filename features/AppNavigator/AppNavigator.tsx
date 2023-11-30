2d574a6a8cb46aaf52d461ff34dedfae91233d75
Voote <kszygy@gmail.com>
2023-11-30 13:41:01 +0000

    quickfix and updates

--- features/AppNavigator/AppNavigator.tsx
+++ features/AppNavigator/AppNavigator.tsx
@@ -7,32 +7,30 @@ import { styles } from './AppNavigator.styles';
 
 const Stack = createStackNavigator();
 
-export const AppNavigator = () => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerStyle: styles.headerStyle,
-            headerTitleAlign: 'center',
-            headerTitleStyle: styles.headerTitleStyle,
+export const AppNavigator = () => (
+  <SafeAreaView style={styles.container}>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: styles.headerStyle,
+          headerTitleAlign: 'center',
+          headerTitleStyle: styles.headerTitleStyle,
+        }}
+        initialRouteName="SetSearch"
+      >
+        <Stack.Screen
+          name="SetSearch"
+          options={{
+            title:
+              'Welcome to BrickInspector, your handy list of LEGO sets condition',
+            headerTitle: (props) => (
+              <Text {...props} style={styles.customTitle} />
+            ),
           }}
-          initialRouteName="SetSearch"
-        >
-          <Stack.Screen
-            name="SetSearch"
-            options={{
-              title:
-                'Welcome to BrickInspector, your handy list of LEGO sets condition',
-              headerTitle: (props) => (
-                <Text {...props} style={styles.customTitle} />
-              ),
-            }}
-            component={SetSearch}
-          />
-          <Stack.Screen name="PartsListScreen" component={PartsListScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaView>
-  );
-};
+          component={SetSearch}
+        />
+        <Stack.Screen name="PartsListScreen" component={PartsListScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  </SafeAreaView>
+);
