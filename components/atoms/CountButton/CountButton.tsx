fdf97a787f0786075fe662a74d5ed6b63964ef15
Voote <kszygy@gmail.com>
2024-03-13 13:16:07 +0000

    feat: longpress multiple use added

--- components/atoms/CountButton/CountButton.tsx
+++ components/atoms/CountButton/CountButton.tsx
@@ -1,7 +1,8 @@
 import classNames from 'classnames';
+import React, { FC, useState } from 'react';
 import { GestureResponderEvent, Text, TouchableOpacity } from 'react-native';
 
-export const CountButton: React.FC<CountButtonProps> = ({
+export const CountButton: FC<CountButtonProps> = ({
   label,
   variant = 'default',
   action,
@@ -9,12 +10,30 @@ export const CountButton: React.FC<CountButtonProps> = ({
 }) => {
   const { color: buttonColor, text: buttonTextColor } =
     buttonVariantStyles[variant];
+  const [intervalId, setIntervalId] = useState<number | null>(null);
+
+  const handleLongPress = (event: GestureResponderEvent) => {
+    if (longAction) {
+      longAction(event); // Initial long press action
+      // Start the interval and store its ID
+      const id = setInterval(() => longAction(event), 500);
+      setIntervalId(id);
+    }
+  };
+
+  const handlePressOut = () => {
+    if (intervalId !== null) {
+      clearInterval(intervalId);
+      setIntervalId(null);
+    }
+  };
 
   return (
     <TouchableOpacity
       className={classNames(buttonColor, 'my-1')}
       onPress={action}
-      onLongPress={longAction}
+      onLongPress={handleLongPress}
+      onPressOut={handlePressOut} // Make sure to bind `handlePressOut` to stop the interval
     >
       <Text
         className={classNames(
